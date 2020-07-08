fed6d626e7a0908bc3e0946a2d4d00e7892e6cff
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-07 21:04:03 -0300

    new app function

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,26 +1,29 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
 
 import SearchBar from './search_bar';
-
 import Gif from './gif';
-
 import GifList from './gif_list';
 
-class App extends Component{
-  render(){
+const GIPHY_API_KEY = '1KMPHCBIOe3hOjJwCJQX49sRc6cM0oIm';
+
+class App extends Component {
+
+  render() {
     return (
       <div>
         <div className="left-scene">
           <SearchBar searchFunction={this.search} />
-          <div className="select-gif">
-
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
       </div>
-
     );
   }
 }
-
 
 export default App;
